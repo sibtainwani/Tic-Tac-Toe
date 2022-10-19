54d5a30f755c41a899f6953a49d163c6f2d5d1cf
Sibtain Wani <wanisibu@gmail.com>
2022-10-19 13:41:57 +0530

    fixed directory structure

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-function App() {
-  return (
-   <h1>Tic Tac Toe Game</h1>
-  );
-}
-
-export default App;
---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <h1>Tic Tac Toe Game</h1>;
+}
+
+export default App;
